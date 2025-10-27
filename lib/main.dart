7c5e66f0c518db151fe9d21a610d7392197f9fb5
Sami-Saqr <140464418+Sami-Saqr@users.noreply.github.com>
2025-10-28 02:49:30 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,17 @@
 void main() {
   Human Sami = Human();
 
-  Sami.name = "Sami";
+  Human.name = "Sami";
   Sami.age = 25;
 
-  print('== Hello Class ==\n   Name: ${Sami.name}\n   Age: ${Sami.age}');
+  print('== Hello Class ==\n   Name: ${Human.name}\n   Age: ${Sami.age}');
 }
 
 //this is Basic Class
+// static keyword is used to define class-level properties and methods
+// static properties and methods can be accessed without creating an instance of the class
+// non-static properties and methods are tied to the instance of the class
 class Human {
-  String? name;
+  static String? name;
   int? age;
 }
